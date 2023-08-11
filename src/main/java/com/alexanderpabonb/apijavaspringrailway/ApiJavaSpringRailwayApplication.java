52ed7cce07c7afdff18d9c9fa895364c58e49bf7
Alexander Pabon <alex312-@hotmail.com>
2023-08-11 00:02:21 -0500

    Initial commit

---
+++ src/main/java/com/alexanderpabonb/apijavaspringrailway/ApiJavaSpringRailwayApplication.java
@@ -0,0 +1,11 @@
+package com.alexanderpabonb.apijavaspringrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaSpringRailwayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaSpringRailwayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexanderpabonb/apijavaspringrailway/ApiJavaSpringRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexanderpabonb.apijavaspringrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaSpringRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
